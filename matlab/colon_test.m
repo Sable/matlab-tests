db3ce85d012e2a4244ef27354b415299faad3448
Erick <github@ericklavoie.com>
2016-09-27 11:58:52 +0200

    Removed unused files

--- matlab/colon_test.m
+++
@@ -1,8 +0,0 @@
-function colon_test( x )
-    assert(isequal(0:3, [0,1,2,3]));
-    assert(isequal(0:1:3, [0,1,2,3]));
-    assert(isequal(3:-1:0, [3,2,1,0]));
-    assert(isequal(0:3:5, [0,3]));
-    assert(isequal(5:-3:0, [5,2]));
-end
-
