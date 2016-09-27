716d07aec94f439f327f9632936d5af7daf1709d
Erick <github@ericklavoie.com>
2016-09-27 14:16:25 +0200

    Fixed bugs

--- tests/get_test.m
+++ tests/get_test.m
@@ -1,6 +1,6 @@
 function get_test( x )
-    row_vec = [1;2;3;4];
-    col_vec = [1 2 3 4];
+    row_vec = [1 2 3 4];
+    col_vec = [1;2;3;4];
     mat = [1 2 3;4 5 6];
 
     assert(row_vec(1) == 1);
@@ -22,17 +22,17 @@ function get_test( x )
     assert(col_vec(4,1) == 4);
 
     assert(mat(1) == 1);
-    assert(mat(2) == 2);
-    assert(mat(3) == 3);
-    assert(mat(4) == 4);
-    assert(mat(5) == 5);
+    assert(mat(2) == 4);
+    assert(mat(3) == 2);
+    assert(mat(4) == 5);
+    assert(mat(5) == 3);
     assert(mat(6) == 6);
     
     assert(mat(1,1) == 1);
-    assert(mat(2,1) == 2);
-    assert(mat(3,1) == 3);
-    assert(mat(1,2) == 4);
+    assert(mat(1,2) == 2);
+    assert(mat(1,3) == 3);
+    assert(mat(2,1) == 4);
     assert(mat(2,2) == 5);
-    assert(mat(3,2) == 6);
+    assert(mat(2,3) == 6);
 end
 
